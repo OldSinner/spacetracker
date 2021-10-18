a224961302ad279ecb456b1bddd7bf631c0c6f8c
OldSinner <antylifepatia@gmail.com>
2021-10-18 11:22:42 +0200

    searchbar

--- src/Components/ListLaunch/ListLaunch.jsx
+++ src/Components/ListLaunch/ListLaunch.jsx
@@ -7,6 +7,9 @@ import Api from "../../Globals/Api";
 import Pagination from "@material-ui/lab/Pagination";
 import { Skeleton } from "@mui/material";
 import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@mui/material/TextField";
+import { red } from "@mui/material/colors";
+//-------------------------------------
 const useStyles = makeStyles(() => ({
   ul: {
     "& .MuiPaginationItem-root": {
@@ -22,6 +25,13 @@ export function ListLaunchD({ title }) {
   const [isLoaded, setIsLoaded] = useState(false);
   const [search, setSearch] = useState("");
   const classes = useStyles();
+  var lastTimeout;
+  const handleSearch = (event) => {
+    clearTimeout(lastTimeout);
+    lastTimeout = setTimeout(() => {
+      setSearch(event.target.value);
+    }, 500);
+  };
 
   const handleChange = (event, value) => {
     setPage(value - 1);
@@ -47,7 +57,7 @@ export function ListLaunchD({ title }) {
         setError(error);
         setIsLoaded(true);
       });
-  }, [page, title]);
+  }, [page, title, search]);
 
   if (error) {
     return (
@@ -66,7 +76,7 @@ export function ListLaunchD({ title }) {
               variant="rectangular"
               width={500}
               height={500}
-              x={{
+              sx={{
                 margin: "10px",
               }}
             />
@@ -75,7 +85,7 @@ export function ListLaunchD({ title }) {
               variant="rectangular"
               width={500}
               height={500}
-              x={{
+              sx={{
                 margin: "10px",
               }}
             />
@@ -95,21 +105,64 @@ export function ListLaunchD({ title }) {
   } else {
     return (
       <div className="listLaunch">
-        <div className="detColor fs60 text-al-center">{title} launch</div>
-        <div className="listGrid">
-          <Launch launchInfo={uplaunch.data.results[0]} timmer={timer} />
-          <Launch launchInfo={uplaunch.data.results[1]} timmer={timer} />
-          <Launch launchInfo={uplaunch.data.results[2]} timmer={timer} />
+        <div className="listTitle">
+          <div className="tf"></div>
+          <div className="detColor fs60 text-al-center listT">
+            {title} launch
+          </div>
+          <div className="tf">
+            <TextField
+              onChange={handleSearch}
+              sx={{
+                "& label.Mui-focused": {
+                  color: "var(--detailColor)",
+                },
+                "& label": {
+                  color: "var(--detailColor)",
+                },
+                "& .MuiInput-underline:after": {
+                  borderBottomColor: "var(--detailColor)",
+                },
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderColor: "var(--detailColor)",
+                  },
+                  "&:hover fieldset": {
+                    borderColor: "var(--detailColor)",
+                  },
+                },
+                input: {
+                  color: "var(--firstColor) !important",
+                  borderColor: "var(--firstColor)",
+                },
+              }}
+              id="outlined-basic"
+              label="Search"
+              color="primary"
+            ></TextField>
+          </div>
         </div>
+       
+          {uplaunch?.data?.results[0] ? (
+             <div className="listGrid">
+              <Launch launchInfo={uplaunch.data.results[0]} timmer={timer} />
+              <Launch launchInfo={uplaunch.data.results[1]} timmer={timer} />
+              <Launch launchInfo={uplaunch.data.results[2]} timmer={timer} />
+              </div>
+          ) : (
+            <div className="detColor fs60 text-al-center listT">
+              No results found :/ Sorry
+            </div>
+          )}
         <Pagination
-            count={Math.floor(uplaunch.data.count / 3)}
-            color="primary"
-            showLastButton
-            onChange={handleChange}
-            size="large"
-            page={page + 1}
-            classes={{ ul: classes.ul }}
-          />
+          count={Math.floor(uplaunch.data.count / 3)}
+          color="primary"
+          showLastButton
+          onChange={handleChange}
+          size="large"
+          page={page + 1}
+          classes={{ ul: classes.ul }}
+        />
         {/* <i
           className="fas fa-angle-right fs60 btnHover"
           onClick={() => {
@@ -128,6 +181,11 @@ export function ListLaunchM({ title }) {
   const [uplaunch, setLaunch] = useState([]);
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const classes = useStyles();
+
+  const handleChange = (event, value) => {
+    setPage(value - 1);
+  };
   var timer = title.toLowerCase() === "upcoming" ? true : false;
   useEffect(() => {
     axios
@@ -183,31 +241,22 @@ export function ListLaunchM({ title }) {
   } else {
     return (
       <div className="listLaunch">
-        {page > 0 ? (
-          <i
-            className="fas fa-angle-left fs60 btnHover"
-            onClick={() => {
-              setPage(page - 1);
-              setIsLoaded(false);
-            }}
-          ></i>
-        ) : (
-          <i className="fas fa-angle-left fs60 btnDisable"></i>
-        )}
-
-        <div>
-          <div className="detColor fs45 text-al-center">{title} launch</div>
-          <div className="listGrid">
-            <Launch launchInfo={uplaunch.data.results[0]} timmer={timer} />
-          </div>
+        <div className="listTitle">
+          <div className="detColor fs60 text-al-center">{title} launch</div>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
         </div>
-        <i
-          className="fas fa-angle-right fs60 btnHover"
-          onClick={() => {
-            setPage(page + 1);
-            setIsLoaded(false);
-          }}
-        ></i>
+        <div className="listGrid">
+          <Launch launchInfo={uplaunch.data.results[0]} timmer={timer} />
+        </div>
+        <Pagination
+          count={Math.floor(uplaunch.data.count / 3)}
+          color="primary"
+          showLastButton
+          onChange={handleChange}
+          size="small"
+          page={page + 1}
+          classes={{ ul: classes.ul }}
+        />
       </div>
     );
   }
