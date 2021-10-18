3fa7cbdfe8c0d34dd56996777db2acfca6e57491
OldSinner <antylifepatia@gmail.com>
2021-10-18 11:49:54 +0200

    search bar fixes

--- src/Components/ListLaunch/ListLaunch.jsx
+++ src/Components/ListLaunch/ListLaunch.jsx
@@ -1,14 +1,14 @@
 import "./ListLaunch.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Loading from "../Loading";
 import Launch from "../Launch/Launch";
 import Api from "../../Globals/Api";
 import Pagination from "@material-ui/lab/Pagination";
 import { Skeleton } from "@mui/material";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@mui/material/TextField";
-import { red } from "@mui/material/colors";
+import { Breakpoint, useCurrentWidth } from "react-socks";
+
 //-------------------------------------
 const useStyles = makeStyles(() => ({
   ul: {
@@ -19,21 +19,25 @@ const useStyles = makeStyles(() => ({
 }));
 
 export function ListLaunchD({ title }) {
+  const width = useCurrentWidth();
   const [page, setPage] = useState(0);
   const [uplaunch, setLaunch] = useState([]);
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [search, setSearch] = useState("");
   const classes = useStyles();
+  const divider = width < 900 ? 1 : 3;
   var lastTimeout;
   const handleSearch = (event) => {
     clearTimeout(lastTimeout);
     lastTimeout = setTimeout(() => {
       setSearch(event.target.value);
+      setPage(0);
     }, 500);
   };
 
   const handleChange = (event, value) => {
+    setIsLoaded(false);
     setPage(value - 1);
   };
   var timer = title.toLowerCase() === "upcoming" ? true : false;
@@ -44,8 +48,10 @@ export function ListLaunchD({ title }) {
         Api +
           "/launch/" +
           title.toLowerCase() +
-          "/?limit=3&offset=" +
-          3 * page +
+          "/?limit=" +
+          divider +
+          "&offset=" +
+          divider * page +
           "&search=" +
           search
       )
@@ -57,6 +63,7 @@ export function ListLaunchD({ title }) {
         setError(error);
         setIsLoaded(true);
       });
+    // eslint-disable-next-line
   }, [page, title, search]);
 
   if (error) {
@@ -70,35 +77,50 @@ export function ListLaunchD({ title }) {
       <div className="listLaunch">
         <div className="detColor fs45 text-al-center">{title} launch</div>
         <div className="loading">
-          <div className="listGrid">
-            <Skeleton
-              animation="wave"
-              variant="rectangular"
-              width={500}
-              height={500}
-              sx={{
-                margin: "10px",
-              }}
-            />
-            <Skeleton
-              animation="wave"
-              variant="rectangular"
-              width={500}
-              height={500}
-              sx={{
-                margin: "10px",
-              }}
-            />
-            <Skeleton
-              animation="wave"
-              variant="rectangular"
-              width={500}
-              height={500}
-              sx={{
-                margin: "10px",
-              }}
-            />
-          </div>
+          <Breakpoint large up>
+            <div className="listGrid">
+              <Skeleton
+                animation="wave"
+                variant="rectangular"
+                width={500}
+                height={500}
+                sx={{
+                  margin: "10px",
+                }}
+              />
+              <Skeleton
+                animation="wave"
+                variant="rectangular"
+                width={500}
+                height={500}
+                sx={{
+                  margin: "10px",
+                }}
+              />
+              <Skeleton
+                animation="wave"
+                variant="rectangular"
+                width={500}
+                height={500}
+                sx={{
+                  margin: "10px",
+                }}
+              />
+            </div>
+          </Breakpoint>
+          <Breakpoint medium down>
+            <div className="listGrid">
+              <Skeleton
+                animation="wave"
+                variant="rectangular"
+                width={500}
+                height={500}
+                sx={{
+                  margin: "10px",
+                }}
+              />
+            </div>
+          </Breakpoint>
         </div>
       </div>
     );
@@ -109,53 +131,95 @@ export function ListLaunchD({ title }) {
           <div className="tf"></div>
           <div className="detColor fs60 text-al-center listT">
             {title} launch
+            <Breakpoint medium down >
+            <TextField
+                onChange={handleSearch}
+                defaultValue={search}
+                sx={{
+                  "& label.Mui-focused": {
+                    color: "var(--detailColor)",
+                  },
+                  "& label": {
+                    color: "var(--detailColor)",
+                  },
+                  "& .MuiInput-underline:after": {
+                    borderBottomColor: "var(--detailColor)",
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderColor: "var(--detailColor)",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "var(--detailColor)",
+                    },
+                  },
+                  input: {
+                    color: "var(--firstColor) !important",
+                    borderColor: "var(--firstColor)",
+                  },
+                }}
+                id="outlined-basic"
+                label="Search"
+                color="primary"
+              ></TextField>
+            </Breakpoint>
           </div>
           <div className="tf">
-            <TextField
-              onChange={handleSearch}
-              sx={{
-                "& label.Mui-focused": {
-                  color: "var(--detailColor)",
-                },
-                "& label": {
-                  color: "var(--detailColor)",
-                },
-                "& .MuiInput-underline:after": {
-                  borderBottomColor: "var(--detailColor)",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: "var(--detailColor)",
+            <Breakpoint large up>
+              <TextField
+                onChange={handleSearch}
+                defaultValue={search}
+                sx={{
+                  "& label.Mui-focused": {
+                    color: "var(--detailColor)",
                   },
-                  "&:hover fieldset": {
-                    borderColor: "var(--detailColor)",
+                  "& label": {
+                    color: "var(--detailColor)",
                   },
-                },
-                input: {
-                  color: "var(--firstColor) !important",
-                  borderColor: "var(--firstColor)",
-                },
-              }}
-              id="outlined-basic"
-              label="Search"
-              color="primary"
-            ></TextField>
+                  "& .MuiInput-underline:after": {
+                    borderBottomColor: "var(--detailColor)",
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderColor: "var(--detailColor)",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "var(--detailColor)",
+                    },
+                  },
+                  input: {
+                    color: "var(--firstColor) !important",
+                    borderColor: "var(--firstColor)",
+                  },
+                }}
+                id="outlined-basic"
+                label="Search"
+                color="primary"
+              ></TextField>
+            </Breakpoint>
           </div>
         </div>
-       
-          {uplaunch?.data?.results[0] ? (
-             <div className="listGrid">
-              <Launch launchInfo={uplaunch.data.results[0]} timmer={timer} />
-              <Launch launchInfo={uplaunch.data.results[1]} timmer={timer} />
-              <Launch launchInfo={uplaunch.data.results[2]} timmer={timer} />
+
+        {uplaunch?.data?.results[0] ? (
+          <>
+            <Breakpoint large up>
+              <div className="listGrid">
+                <Launch launchInfo={uplaunch.data.results[0]} timmer={timer} />
+                <Launch launchInfo={uplaunch.data.results[1]} timmer={timer} />
+                <Launch launchInfo={uplaunch.data.results[2]} timmer={timer} />
               </div>
-          ) : (
-            <div className="detColor fs60 text-al-center listT">
-              No results found :/ Sorry
-            </div>
-          )}
+            </Breakpoint>
+            <Breakpoint medium down>
+              <Launch launchInfo={uplaunch.data.results[0]} timmer={timer} />
+            </Breakpoint>
+          </>
+        ) : (
+          <div className="detColor fs60 text-al-center listT">
+            No results found :/ Sorry
+          </div>
+        )}
         <Pagination
-          count={Math.floor(uplaunch.data.count / 3)}
+          count={Math.floor(uplaunch.data.count / divider)}
           color="primary"
           showLastButton
           onChange={handleChange}
@@ -170,93 +234,6 @@ export function ListLaunchD({ title }) {
             setIsLoaded(false);
           }}
         ></i> */}
-      </div>
-    );
-  }
-}
-
-// MOBILE -------------------------------------------------------
-export function ListLaunchM({ title }) {
-  const [page, setPage] = useState(0);
-  const [uplaunch, setLaunch] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const classes = useStyles();
-
-  const handleChange = (event, value) => {
-    setPage(value - 1);
-  };
-  var timer = title.toLowerCase() === "upcoming" ? true : false;
-  useEffect(() => {
-    axios
-      .get(
-        Api + "/launch/" + title.toLowerCase() + "/?limit=1&offset=" + 1 * page
-      )
-      .then((res) => {
-        setLaunch(res);
-        setIsLoaded(true);
-      })
-      .catch((error) => {
-        setError(error);
-        setIsLoaded(true);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
-
-  if (error) {
-    return (
-      <div className="listLaunch">
-        <div className="slidder subtitle">Problem Occured! Sorry!</div>
-      </div>
-    );
-  } else if (!isLoaded) {
-    return (
-      <div className="listLaunch">
-        {page > 0 ? (
-          <i
-            className="fas fa-angle-left fs60 btnHover"
-            onClick={() => {
-              setPage(page - 1);
-              setIsLoaded(false);
-            }}
-          ></i>
-        ) : (
-          <i className="fas fa-angle-left fs60 btnDisable"></i>
-        )}
-        <div>
-          <div className="detColor fs45 text-al-center">{title} launch</div>
-          <div className="listGrid">
-            <Loading />
-          </div>
-        </div>
-        <i
-          className="fas fa-angle-right fs60 btnHover"
-          onClick={() => {
-            setPage(page + 1);
-            setIsLoaded(false);
-          }}
-        ></i>
-      </div>
-    );
-  } else {
-    return (
-      <div className="listLaunch">
-        <div className="listTitle">
-          <div className="detColor fs60 text-al-center">{title} launch</div>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </div>
-        <div className="listGrid">
-          <Launch launchInfo={uplaunch.data.results[0]} timmer={timer} />
-        </div>
-        <Pagination
-          count={Math.floor(uplaunch.data.count / 3)}
-          color="primary"
-          showLastButton
-          onChange={handleChange}
-          size="small"
-          page={page + 1}
-          classes={{ ul: classes.ul }}
-        />
       </div>
     );
   }
--- src/Pages/LaunchList.jsx
+++ src/Pages/LaunchList.jsx
@@ -1,19 +1,12 @@
-import { ListLaunchD, ListLaunchM } from "../Components/ListLaunch/ListLaunch";
-import { Breakpoint } from "react-socks";
-import Search from "../Components/Search";
+import { ListLaunchD } from "../Components/ListLaunch/ListLaunch";
+
 
 export default function LaunchList() {
   return (
     <div>
-      <Search/>
-      <Breakpoint large up>
+  
         <ListLaunchD title="Upcoming" />
-        <ListLaunchD title="Previous" />
-      </Breakpoint>
-      <Breakpoint medium down>
-        <ListLaunchM title="Upcoming" />
-        <ListLaunchM title="Previous" />
-      </Breakpoint>
+        <ListLaunchD title="Previous" />  
     </div>
   );
 }
