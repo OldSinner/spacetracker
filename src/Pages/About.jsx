7f67dd7e12756132170cf5a54289914b91bc2f22
OldSinner <antylifepatia@gmail.com>
2021-11-04 10:13:56 +0100

    about page + some fixes

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,16 @@
 
 const About = () => {
     return (
-        <div className="div">
-            
+        <div className="pageWrap">
+            <div className="pageBody">
+                <div className="itemPage">
+                    SpaceTracker
+                </div>
+                <div className="itemPage">
+                    Autor: Przysiwek Maksymilian
+                </div>
+               
+            </div>
         </div>
     )
 }
